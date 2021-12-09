8ac5b771d0b56240f5cb5e774d56de6c3b39fdc1
Avoler0 <jbu1391@gmail.com>
2021-12-09 13:13:21 +0900

    noteEditModify

--- public/js/NoteScript.js
+++ public/js/NoteScript.js
@@ -1,11 +1,12 @@
-const NoteField = document.querySelector(".Note-Edit-Field"); // 메모장 에디터
-const NoteDataList = document.querySelector(".List-Note-Data");
+const noteField = document.querySelector(".Note-Edit-Field"); // 메모장 에디터
+const noteDataList = document.querySelector(".List-Note-Data");
 let NoteCount = 0;
-let cnt = 0;
+let SaveCount = 0;
 
 function init(){
   
 }
+
 function ClickPlusNote(){
   if(!document.querySelector(".EditNote")){
     CreateNote();
@@ -13,26 +14,29 @@ function ClickPlusNote(){
   ReNote();
 }
 }
+
 function CreateNote(){
-  let NoteTitleDiv = document.createElement('div');
-  let NoteDiv = document.createElement('div');
-  NoteTitleDiv.className="EditNoteTitle";
-  NoteTitleDiv.innerHTML="새로운 메모"+NoteCount;
-  NoteDiv.className ="EditNote";
-  NoteDiv.setAttribute('contenteditable','true');
-  NoteDiv.setAttribute('onkeydown','KeyEvent(event)');
-  NoteField.appendChild(NoteTitleDiv);
-  NoteField.appendChild(NoteDiv);
+  let noteTitleDiv = document.createElement('div');
+  let noteDiv = document.createElement('div');
+  noteTitleDiv.className="EditNoteTitle";
+  noteTitleDiv.innerHTML="새로운 메모"+NoteCount;
+  noteDiv.className ="EditNote";
+  noteDiv.setAttribute('contenteditable','true');
+  noteDiv.setAttribute('onkeydown','KeyEvent(event)');
+  noteField.appendChild(noteTitleDiv);
+  noteField.appendChild(noteDiv);
 }
+
+
 function ReNote(){
   let OkNo = confirm("새로 만드시겠습니까?");
   if(OkNo)
   {
     if(NoteCount!==0)
       NoteCount--;
-    while(NoteField.hasChildNodes()) // 남아있는 메모장 정리
+    while(noteField.hasChildNodes()) // 남아있는 메모장 정리
       {
-    NoteField.removeChild(NoteField.firstChild);
+        noteField.removeChild(noteField.firstChild);
      }
     CreateNote();
     
@@ -42,42 +46,68 @@ function ReNote(){
 }
 function NoteSave(){
   
-  
+  ++NoteCount;
   if(confirm("저장하시겠습니까"))
   {
-  let NoteData = document.querySelector(".EditNote");
-  let m_ctrlNoteDataList = document.querySelector(".List-Note-Data");
-  let m_NoteDataList = document.createElement('div');
-  m_NoteDataList.className = "Note-List-Data"+NoteCount;
-  let temp = NoteData.innerHTML.split("<div>",1); // 메모장의 첫번째줄 저장
-  m_NoteDataList.innerHTML += "<a href ='#' onclick='ShowNote("+cnt+")'></a>"; // 노트 리스트에 저장된 메모 태그추가
-  localStorage.setItem('SaveDataNote'+NoteCount,NoteData.innerHTML);
-  localStorage.setItem('SaveNoteCnt',NoteCount);
-  m_ctrlNoteDataList.appendChild(m_NoteDataList); // 노트리스트에 자식태그로 추가
-  let aTag = NoteDataList.querySelector("a"); // 저장된 메모 태그를 받아옴
-  aTag.innerHTML = temp;  // 태그의 inner에 메모장의 첫번째 줄 저장
-  ++NoteCount;
-  while(NoteField.hasChildNodes()) // 남아있는 메모장 정리
-  {
-    NoteField.removeChild(NoteField.firstChild);
-  }
+    
+  let editNoteData = document.querySelector(".EditNote");
+  let noteDataName = document.createElement('div');
+  let temp = editNoteData.innerHTML.split('<div>',1);
+    if(editNoteData.innerHTML.length > 2)
+    {
+      localStorage.setItem('saveEditData'+NoteCount,editNoteData.innerHTML); // 메모 내용 저장
+      noteDataName.setAttribute("id","listSave"+NoteCount); // 리스트에 저장할 아이디 설정
+      noteDataName.className="showTag";               // 리스트에 저장할 클래스 설정
+      noteDataName.innerHTML = "<a href ='#' onclick='ShowNote("+NoteCount+")'></a>"; // 리스트에 저장된 메모 태그추가
+      let aTag = noteDataName.querySelector("a");
+      aTag.innerHTML = temp;
+      
+      noteDataList.appendChild(noteDataName);
+      //localStorage.setItem('listCount',NoteCount);
+      while(noteField.hasChildNodes()) // 남아있는 메모장 정리
+      {
+        noteField.removeChild(noteField.firstChild);
+      }
+    }
+    else{
+      alert("저장할 것이 없습니다.");
+      return;
+    }
+  //let temp = editNoteData.textContent;
+
+  
+  
   }
   else{
     return;
   }
 }
-//SaveIcon.addEventListener("click",ClickPlusNote);
-function ShowNote(count){
-  let ShowData = localStorage.getItem('SaveDataNote'+count);
-  let SaveNoteDiv = document.createElement('div');
 
-  SaveNoteDiv.className = "ShowData"+count;
-  SaveNoteDiv.innerHTML = 1234;
+function ShowNote(count){
+  while(noteField.hasChildNodes()) // 남아있는 메모장 정리
+  {
+    noteField.removeChild(noteField.firstChild);
+  }
+  let showData = localStorage.getItem('saveEditData'+count);
+  let NoteTitleDiv = document.createElement('div');
+  let NoteDiv = document.createElement('div');
+
+  NoteTitleDiv.className="EditNoteTitle";
+  NoteTitleDiv.innerHTML="새로운 메모"+NoteCount;
+  NoteDiv.className ="EditNote";
+  NoteDiv.setAttribute('contenteditable','true');
+  NoteDiv.innerHTML = showData;
+  //NoteDiv.setAttribute('onkeydown','KeyEvent(event)');
+  //NoteDiv.innerHTML = localStorage.getItem
+  noteField.appendChild(NoteTitleDiv);
+  noteField.appendChild(NoteDiv);
   
-  NoteField.appendChild(SaveNoteDiv);
+  saveNoteData = document.querySelector("#NoteData"+count)
+  noteField.appendChild(saveNoteDiv);
 }
-function KeyEvent(Event){
+/*function KeyEvent(Event){
+  
   if(Event.keyCode==13){
     NoteSave();
   }
-}
+}*/
