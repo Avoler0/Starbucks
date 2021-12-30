cd8b34d3393f4fc391a8abd0c0a3adbde21b86fa
Avoler0 <jbu1391@gmail.com>
2021-12-30 17:49:29 +0900

    StarbucksLayout

--- public/js/Layout.js
+++ public/js/Layout.js
@@ -1,15 +1,17 @@
 
 const inner = {
   $gnbNav01: document.querySelector(".gnb_nav01"),
-  $gnbNav01a: document.querySelector(".gnb_nav01 a")
-
-
+  $gnbNav01a: document.querySelector(".gnb_nav01 a"),
+  $gnbSubWrap: document.querySelector(".gnb_sub_wrap"),
+  $subGnbNavInner:document.querySelector(".sub_gnb_nav_inner"),
 };
 
 
-inner.$gnbNav01.addEventListener('mouseover',() =>{
+inner.$subGnbNavInner.addEventListener('mouseover',() =>{
   inner.$gnbNav01a.classList.add('on');
+  inner.$gnbSubWrap.style.display="block";
 });
-inner.$gnbNav01.addEventListener('mouseout',() =>{
+inner.$subGnbNavInner.addEventListener('mouseout',() =>{
   inner.$gnbNav01a.classList.remove('on');
+  //inner.$gnbSubWrap.style.display="none";
 });
